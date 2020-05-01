47f6c16afc89a1e86f438206a10711aa02df7730
Georgi Petrov <54109556+Georgi051@users.noreply.github.com>
2020-05-01 18:46:26 +0300

    g

--- Java MVC/HEROES/src/main/java/application/heroes/domain/entities/Gender.java
+++
@@ -1,5 +0,0 @@
-package application.heroes.domain.entities;
-
-public enum Gender {
-    MALE,FEMALE
-}
--- Java MVC/HEROES/src/main/java/application/heroes/domain/entities/Slot.java
+++
@@ -1,5 +0,0 @@
-package application.heroes.domain.entities;
-
-public enum Slot {
-    WEAPON, HELMET, PAULDRON, PADS, GAUNTLETS
-}
